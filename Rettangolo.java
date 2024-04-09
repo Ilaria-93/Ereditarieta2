3077206803eb4fbdf18ff13e6080a710a52e466e
Ilaria-93 <150346358+Ilaria-93@users.noreply.github.com>
2024-04-09 12:57:47 +0200

    Rettangolo.java

--- Rettangolo.java
+++ Rettangolo.java
@@ -1,12 +1,36 @@
 public class Rettangolo extends Forma{
 
     //proprietà del rettangolo
-    private int base;
-    private int altezza;
+    private double base;
+    private double altezza;
+
+    //definisco costruttore
+    public Rettangolo(double base, double altezza) {
+        this.base = base;
+        this.altezza = altezza;
+    }
+
+    //definisco getters e setters
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getAltezza() {
+        return altezza;
+    }
+
+    public void setAltezza(double altezza) {
+        this.altezza = altezza;
+    }
 
     //calcola area del rettangolo
-    public void calcolaArea (int base, int altezza) {
-        int area = base * altezza;
-        System.out.println(area);
+    public double calcolaArea () {
+        double area = base * altezza;
+        System.out.print("L'area del rettangolo è ");
+        return area;
     }
 }
